dcc385283a7c8a46f2b5eab1f75f4e6b9d1da336
Kevin <kd20958@email.vccs.edu>
2014-11-07 15:50:06 -0500

    initial

--- CycleInput/src/Cycle.java
+++ CycleInput/src/Cycle.java
@@ -8,23 +8,23 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Cycle {
-	public static double numberOfWheels;
+	public static double numberOfWheels; //initialize values
 	public static double weight;
 	
-	public Cycle(double numberOfWheels, double weight){
+	public Cycle(double numberOfWheels, double weight){ //constructor
 		this.numberOfWheels=numberOfWheels;
 		this.weight=weight;
 	}
 	
 	public String toString() {
-		return "Cycle [numberOfWheels= " + numberOfWheels + ", weight= " + weight + "]";
+		return "Cycle [numberOfWheels= " + numberOfWheels + ", weight= " + weight + "]"; //to string
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException   {
 		// TODO Auto-generated method stub
 		Scanner input = null;
-		try {
-			File file = new File("C:/Users/Kevin/Desktop/Workspace/CycleInput/CycleIn.txt");
+		try { //try catch methods
+			File file = new File("C:/Users/Kevin/Desktop/Workspace/CycleInput/CycleIn.txt"); //calls txt location
 			input = new Scanner(file);
 		}
 		catch(FileNotFoundException e) {
@@ -32,7 +32,7 @@ public class Cycle {
 			throw e;
 		}
 		
-		while(input.hasNextDouble()) {
+		while(input.hasNextDouble()) { //set wheel and weight values
 			 numberOfWheels=input.nextDouble();
 		}
 		
@@ -40,7 +40,7 @@ public class Cycle {
 			 weight=input.nextDouble();
 		}
 		
-	 Cycle c = new Cycle(numberOfWheels, weight);
+	 Cycle c = new Cycle(numberOfWheels, weight); //sets new file
 	 System.out.println(c.toString());
 	}
 
